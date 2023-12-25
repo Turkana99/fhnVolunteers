94c2a1e43682e8198caa3a8d37cac4c3ee732fad
Turkana Mammadova <mmmmadova.turkana@gmail.com>
2023-12-25 18:07:51 +0400

    Add sidebar changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Output() toggleSidebarEvent = new EventEmitter<boolean>();
 
+  toggleSidebar(): void {
+    this.toggleSidebarEvent.emit(true); // Pass true to show the sidebar
+  }
 }
